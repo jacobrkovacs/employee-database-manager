a5feecc9cb38309dcfdf13a33af24110a5a91760
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-01 12:28:43 -0700

    adds schema.sql and seeds.sql for db

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (department_name)
+VALUES ("Customer Service"),
+       ("Sales"),
+       ("Research and Development"),
+       ("Legal"),
+       ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Help Desk", 60000, 1),
+       ("Live Chat Rep", 50000, 1),
+       ("Junior Sales Rep", 70000, 2),
+       ("Senior Sales Rep", 85000, 2),
+       ("Researcher", 80000, 3),
+       ("Developer", 80000, 3),
+       ("In-house Council", 85000, 4),
+       ("Contracts", 75000, 4),
+       ("Accountant", 75000, 5),
+       ("Payroll", 70000, 5);
